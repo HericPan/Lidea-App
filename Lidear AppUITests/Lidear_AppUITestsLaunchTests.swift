a8028a11d1c2f85a766e9d801a326c2189c90046
Heric Mac <1214423184@qq.com>
2022-03-20 14:44:43 +0800

    Initial Commit

---
+++ Lidear AppUITests/Lidear_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lidear_AppUITestsLaunchTests.swift
+//  Lidear AppUITests
+//
+//  Created by 潘若淮 on 2022/3/20.
+//
+
+import XCTest
+
+class Lidear_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
